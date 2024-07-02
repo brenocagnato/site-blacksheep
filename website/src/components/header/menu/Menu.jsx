435da03c1912498fe7715ad7d42f1dcd25b36839
brenocagnato <122325576+brenocagnato@users.noreply.github.com>
2024-07-02 01:29:39 -0300

    view:Add menu navbar

--- website/src/components/header/menu/Menu.jsx
+++ website/src/components/header/menu/Menu.jsx
@@ -2,24 +2,44 @@ import "../menu/menu.css"
 
 export default function Menu() {
     return(
-       <nav>
-        <div className="info">
-            <p>Black Sheep.2024</p>
-        </div>
-        <div className="logo">
-            <p><a href="#"></a></p>
-        </div>
-        <div className="toogle-btn">
-            <button class="burguer"></button>
-        </div>
-       </nav>
-
-       <div className="overlay">
-        <div className="overlay-menu">
-            <div className="menu-item">
-                
+       <><nav>
+            <div className="info">
+                <p>02.2024</p>
             </div>
-        </div>
-       </div>
+            <div className="logo">
+                <p><a href="#"><img src="D:\site-blacksheep\website\src\assets" alt="Black Sheep Filmes" /></a></p>
+            </div>
+            <div className="toogle-btn">
+                <button class="burguer"></button>
+            </div>
+        </nav><div className="overlay">
+                <div className="overlay-menu">
+                    <div className="menu-item">
+                        <p id="active"><a href="#">Home</a></p>
+                    </div>
+                    <div className="menu-item">
+                        <p><a href="#">Portfolio</a></p>
+                    </div>
+                    <div className="menu-item">
+                        <p><a href="#">About</a></p>
+                    </div>
+                    <div className="menu-item">
+                        <p><a href="#">Blog</a></p>
+                    </div>
+                    <div className="menu-item">
+                        <p><a href="#">Contact</a></p>
+                    </div>
+
+                    <div className="sub-nav">
+                        <p><a href="#">Instagram</a></p>
+                        <p>.</p>
+                        <p><a href="#">Vimeo</a></p>
+                        <p>.</p>
+                        <p><a href="#">Youtube</a></p>
+                        <p>.</p>
+                        <p><a href="#">LinkedIn</a></p>
+                    </div>
+                </div>
+            </div></>
     )
 }
